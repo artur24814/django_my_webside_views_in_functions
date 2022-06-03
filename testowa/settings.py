d0a76cc832bf055d521cd046df6b811321101a2d
artur24814 <97242088+artur24814@users.noreply.github.com>
2022-06-03 20:06:50 +0200

    Update settings.py

--- testowa/settings.py
+++ testowa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9bpj+ta$c=voz9ye+*3^cppe4e0!@(zuap2v0w)su1kb%-wfv3'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
